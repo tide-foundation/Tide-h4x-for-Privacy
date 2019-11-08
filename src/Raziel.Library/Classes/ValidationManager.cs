6a6a397f8c590527aacb846e2ebde851a08ed733
José Lobo <jose@tide.org>
2019-11-08 13:26:04 +1000

    Try the the correct size of the fakes

--- src/Raziel.Library/Classes/ValidationManager.cs
+++ src/Raziel.Library/Classes/ValidationManager.cs
@@ -37,8 +37,10 @@ namespace Raziel.Library.Classes {
             var size = (shareBytes.Length - 1) / 4;
 
             using (var hmac = size <= 32 ? new HMACSHA256(oAuthBytes) : new HMACSHA512(oAuthBytes) as HMAC) {
-                var fakeSecret = hmac.ComputeHash(input.Concat(shareBytes).ToArray()).Take(size);
-                var fakeShare = shareBytes.Take(shareBytes.Length - size * 2).Concat(fakeSecret).Concat(shareBytes.Skip(shareBytes.Length - size));
+                var fakeSecret = new BigInteger(hmac.ComputeHash(input.Concat(shareBytes).ToArray()).Take(size).ToArray(), true);
+                var fakeBytes = (fakeSecret >> 1).ToByteArray(true);
+
+                var fakeShare = shareBytes.Take(shareBytes.Length - size * 2).Concat(fakeBytes).Concat(shareBytes.Skip(shareBytes.Length - size));
                 return Convert.ToBase64String(fakeShare.ToArray());
             }
         }
