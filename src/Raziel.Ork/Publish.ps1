d87d6acd3d317c7849aba571cbe5473e903e8db6
Matthew Spencer <thrakmar@gmail.com>
2020-05-18 12:44:36 +1000

    Added an ork automation deploy script.

--- src/Raziel.Ork/Publish.ps1
+++ src/Raziel.Ork/Publish.ps1
@@ -1,3 +1,5 @@
+az login
+
 $location = Get-Location
 $location = $location.Path
 
@@ -6,7 +8,7 @@ $restartApp = {
     Restart-AzureRmWebApp -ResourceGroupName Raziel -Name raziel-ork-$number
 }
 
-For ($i = 1; $i -le 25; $i++) {
+For ($i = 1; $i -le 10; $i++) {
     $pwd = az webapp deployment list-publishing-profiles --name raziel-ork-$i --resource-group Raziel --query '[].userPWD' -o tsv
     $pwd = $pwd[0];
     $project = -join ($location, '\Raziel.Ork.csproj');
@@ -22,3 +24,10 @@ For ($i = 1; $i -le 25; $i++) {
         Write-Host "Ork" $i "failed to deploy" -ForegroundColor Red
     }
 }
+
+
+
+
+
+
+
